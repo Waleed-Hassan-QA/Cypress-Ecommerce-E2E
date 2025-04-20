3883cfb698f482a8cb2cca0928b6e5bc2b7d67e1
Waleed Hassan <waleedhassan909@gmail.com>
2025-04-20 12:26:35 +0500

    eCommerce Test Automation

---
+++ cypress/e2e/pages/registerPage.js
@@ -0,0 +1,49 @@
+export class registerPage{
+
+locators = {
+      
+    firstname: '#firstname',
+    lastname: '#lastname',
+    email: '#email_address',
+    password: '#password',
+    confirmPassword: '#password-confirmation',
+    createButton: 'button[title="Create an Account"]',
+    verifySuccessMessage: 'div[data-bind="html: $parent.prepareMessageForHtml(message.text)"]'
+
+}
+
+visitRegisterPage(){
+    cy.visit(Cypress.env('URL')) 
+}
+
+enterFirstName(fname){
+    cy.get(this.locators.firstname).type(fname)
+}
+
+enterLastName(lname){
+    cy.get(this.locators.lastname).type(lname)
+}
+
+enterEmail(email){
+    cy.get(this.locators.email).type(email)
+}
+
+
+enterPassword(password){
+    cy.get(this.locators.password).type(password)
+}
+
+enterConfirmPassword(c_password){
+    cy.get(this.locators.confirmPassword).type(c_password)
+}
+
+CreateAccount(){
+    cy.get(this.locators.createButton).click()
+}
+
+getSuccessMessage(){
+    return cy.get(this.locators.verifySuccessMessage)
+}
+
+
+}
---
+++ cypress/e2e/testfiles/registerTest.cy.js
@@ -0,0 +1,25 @@
+import { registerPage } from "../pages/registerPage"
+import registerData from "../../fixtures/registerData.json"
+
+const registerObj = new registerPage()
+
+describe('Registration flow',()=>{
+
+
+    it('create a new user account',()=>{
+      
+      registerObj.visitRegisterPage()
+      registerObj.enterFirstName(registerData.fname)
+      registerObj.enterLastName(registerData.lname)
+      registerObj.enterEmail(registerData.email)
+      registerObj.enterPassword(registerData.password)
+      registerObj.enterConfirmPassword(registerData.confirmPassword)
+      registerObj.CreateAccount()
+      registerObj.getSuccessMessage().should('have.text',registerData.expectedSuccessMessage)
+
+
+    })
+
+
+
+})
